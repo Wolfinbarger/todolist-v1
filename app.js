313143062a1b7102cd83d7d388ec5f24a44d23ee
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-07-05 21:01:04 -0500

    Change var to let

--- app.js
+++ app.js
@@ -7,26 +7,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
 
